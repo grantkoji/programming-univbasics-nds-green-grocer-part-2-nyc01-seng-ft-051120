4678f1bcaf9946c30bc8091db4621071102ef46f
Grant Yoshitsu <grantkoji@gmail.com>
2020-05-08 23:57:33 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,44 @@
 require_relative './part_1_solution.rb'
+require 'pry'
+
+
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+    consolidated_cart = cart
+    coupons.each do |coupon_info|
+      binding.pry
+    coupon_price = coupon_info[:cost] / coupon_info[:num]
+    coupon_name = "#{coupon_info[:item]} W/ COUPON"
+    coupon_count = coupon_info[:num]
+    consolidated_cart.each do |consolidated_cart_info|
+      consolidated_count = consolidated_cart_info[:count|
+        if coupon_info[:item] == consolidated_cart_info[:item]
+          if consolidated_count - coupon_count == 0
+            consolidated_cart_info[:count] = 0
+            consolidated_cart << { :item => coupon_name,
+                                   :price => coupon_price,
+                                   :clearance => consolidated_cart_info[:clearance],
+                                   :count => coupon_count
+                                 }
+          elsif consolidated_count - coupon_count < 0
+            consolidated_cart_info[:count] = 0
+            consolidated_cart << { :item => coupon_name,
+                                   :price => coupon_price,
+                                   :clearance => consolidated_cart_info[:clearance],
+                                   :count => coupon_count - consolidated_count
+                                 }
+          else
+            consolidated_cart_info[:count] = consolidated_count - coupon_count
+            consolidated_cart << { :item => coupon_name,
+                                   :price => coupon_price,
+                                   :clearance => consolidated_cart_info[:clearance],
+                                   :count => coupon_count
+                                 }
+          end
+        end
+    end
+  end
+  consolidated_cart
 end
 
 def apply_clearance(cart)
