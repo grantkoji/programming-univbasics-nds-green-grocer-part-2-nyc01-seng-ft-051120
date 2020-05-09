a2cd0a68da97eb9432d37da77c07a1893ade3d27
Grant Yoshitsu <grantkoji@gmail.com>
2020-05-09 00:44:18 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -13,7 +13,6 @@ def apply_coupons(cart, coupons)
                 count: coupon[:num]
               }
       item_with_coupon[:count] -= coupon[:num]
-      binding.pry
     end
   end
   cart
@@ -21,47 +20,17 @@ end
 
 
 
-#def apply_coupons(cart, coupons)
-#    consolidated_cart = cart
-#    coupons.each do |coupon_info|
-#    coupon_price = coupon_info[:cost] / coupon_info[:num]
-#    coupon_name = "#{coupon_info[:item]} W/ COUPON"
-#    coupon_count = coupon_info[:num]
-#    consolidated_cart.each do |consolidated_cart_info|
-#      consolidated_count = consolidated_cart_info[:count|
-#        if coupon_info[:item] == consolidated_cart_info[:item]
-#          if consolidated_count - coupon_count == 0
-#            consolidated_cart_info[:count] = 0
-#            consolidated_cart << { :item => coupon_name,
-#                                   :price => coupon_price,
-#                                   :clearance => consolidated_cart_info[:clearance],
-#                                   :count => coupon_count
-#                                 }
-#          elsif consolidated_count - coupon_count < 0
-#            consolidated_cart_info[:count] = 0
-#            consolidated_cart << { :item => coupon_name,
-#                                   :price => coupon_price,
-#                                   :clearance => consolidated_cart_info[:clearance],
-#                                   :count => coupon_count - consolidated_count
-#                                 }
-##          else
-  #          consolidated_cart_info[:count] = consolidated_count - coupon_count
-  #          consolidated_cart << { :item => coupon_name,
-  #                                 :price => coupon_price,
-  #                                 :clearance => consolidated_cart_info[:clearance],
-  #                                 :count => coupon_count
-  #                               }
-  #        end
-  #      end
-  #    end
-  #end
-  #consolidated_cart
-#end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
+  cart.map do |cart_info|
+    if cart_info[:clearance]
+      cart_info[:price] = (cart_info[price] * .8).round(2)
+    end
+  end
+
+  cart
   # REMEMBER: This method **should** update cart
+  
 end
 
 def checkout(cart, coupons)
